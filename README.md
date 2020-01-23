a74958478f286140e0a5463f01eeff5d23ddf09c
Lucas Calje <lucas@Lucass-MacBook-Pro.local>
2020-01-23 22:21:32 +0100

    chore: setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # flow-based-programming
 Flow based programming library
+
+### Setup
+
+Add the dependency manually to package.json and then (in the root) run
+
+    $> lerna bootstrap
---
+++ packages/svelte/src/main.js
@@ -0,0 +1,19 @@
+import App from './App.svelte';
+
+// import { defineCustomElements } from '@scaljeri/fbp/loader';
+// import { applyPolyfills, defineCustomElements } from '@scaljeri/fbp/loader';
+
+// Bind the custom elements to the window object
+// applyPolyfills().then(() => {
+	// defineCustomElements(window);
+//   });
+
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+export default app;
